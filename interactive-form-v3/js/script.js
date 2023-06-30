6fada77b73201567b92376ab0300fa0d249c0c1a
ccyr <ccyrius01@gmail.com>
2023-06-29 20:14:41 -0600

    script start up onload

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,11 @@
-alert("JS connected");
+// alert("JS connected");
+const name = document.getElementById("name");
+const title = document.getElementById("title");
+const otherJobRole = document.getElementById("other-job-role");
+window.onload = () => {
+    name.focus();
+    otherJobRole.hidden = true;
+}
+title.addEventListener("change", e => {
+    alert("eventListening");
+})
