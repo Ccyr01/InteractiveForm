5d50d16b806bc3325a099593267a4c5256b95354
ccyr <ccyrius01@gmail.com>
2023-07-02 23:16:02 -0600

    dataset

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -8,6 +8,9 @@ const title = document.getElementById("title");
 const color = document.getElementById("color");
 const shirtDesigns =document.getElementById("shirt-designs");
 const otherJobRole = document.getElementById("other-job-role");
+const option = document.getElementById("color").childNodes;
+console.log(option[1].dataset.theme);
+
 window.onload = () => {
     //on page load highlight the name box
     name.focus();
@@ -29,20 +32,60 @@ title.addEventListener("change", e => {
 //     color.hidden = true;
 // }
 shirtDesigns.addEventListener("change", e => {
-    let eTarget = e.target.value;
-    if(eTarget === 'js puns'){
+    let eTarget = e.target;
+    console.log(option[1].dataset.theme);
+
+    // for(let i = 2; i < option.length; i++){
+    //     if(option[i].dataset){
+    //         console.log(option[i]);
+    //         console.log(option[i].dataset.theme);
+    //         // option[i].hidden = true;
+    //         // console.log(i);
+    //     }
+    // }
+    // console.log(eTarget.childNodes);
+    if(eTarget.value === 'js puns'){
         color.hidden = false;
+        for(let i = 2; i < option.length; i++){
+            if(option[i].dataset){
+                if(option[i].dataset.theme === 'heart js'){
+                console.log(option[i]);
+                console.log(option[i].dataset.theme);
+                option[i].hidden = true;
+                // option[i].hidden = true;
+                // console.log(i);
+                }
+            }
+        } 
+        // }if(eTarget.value === option)
+        // // option[2].hidden = true;
+        // // option[3].hidden = true;
+        // // option[4].hidden = true;
+
+        // // console.log()
+        
         
     }
-    else if(eTarget ==='heart js'){
+        
+    else if(eTarget.value ==='heart js'){
         color.hidden = false;
+        for(let i = 2; i < option.length; i++){
+            if(option[i].dataset){
+                if(option[i].dataset.theme === 'js puns'){
+                console.log(option[i]);
+                console.log(option[i].dataset.theme);
+                option[i].hidden = true;
+                // option[i].hidden = true;
+                // console.log(i);
+                }
+            }
+        }
     }
 })
 color.addEventListener("change", e => {
-    console.log(e.target);
     if(e.target.value === 'darkslategrey'){
         console.log(e.target.value);
-        console.log(e.target.data-theme);
+        console.log(e.target.dataset.theme);
 
         console.log("true");
     }
